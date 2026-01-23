f442b258632ac7366e6785e1426204f05d7c3d00
Vilela <guilhermevilela@hubert.com.br>
2026-01-22 21:12:05 -0300

    feat: new-input-component

---
+++ src/components/Reuse/Input/index.tsx
@@ -0,0 +1,31 @@
+import type { InputHTMLAttributes } from "react";
+import OutlineIconError from "../../../assets/alert-circle.svg";
+import { IconError, InputContainer, InputContent, InputError } from "./styles";
+
+interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  children: React.ReactNode;
+  error?: string;
+}
+
+export function InputField({ error, children, ...props }: InputFieldProps) {
+  return (
+    <InputContainer>
+      <label>{children}</label>
+
+      <InputContent>
+        <input {...props} />
+        {/* se tiver error aparece isso... e nao tiver outro retorno = && */}
+
+        {error && (
+          <InputError>
+            <IconError src={OutlineIconError} />
+            <p>{error}</p>
+          </InputError>
+        )}
+
+        {/* se nao tiver if, e ESPERAR  O ELSE = ? :  */}
+        {/* {error ? "BATATA" : "COGUMELO"} */}
+      </InputContent>
+    </InputContainer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-import { Container, LeftContainer, Providers, ProvidersTitle } from "./styles";
+import { Left } from "../components/Left";
+import { Right } from "../components/Right";
+import { Container } from "./styles";
 
 export function HomePage() {
   return (
     <Container>
-      <LeftContainer>
-        <Providers>
-          <ProvidersTitle>
-            <h1>Cadastro de fornecedores</h1>
-            <p>Preencha o formulário abaixo para cadastrar seus fornecedores</p>
-          </ProvidersTitle>
-        </Providers>
-      </LeftContainer>
-      <div>DIREITA</div>
+      <Left />
+      <Right />
     </Container>
   );
 }
